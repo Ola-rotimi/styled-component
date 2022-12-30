93dc56320a5bbcb8075a6524146a063eee651892
Olarotimi Adamson <olarotimiadamson@yahoo.com>
2022-12-30 01:23:40 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Button>Click me</Button>
+      <Button>Default</Button>
+      <Button primary>Primary</Button>
+      <Button secondary>Secondary</Button>
     </div>
   );
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,17 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-export const Button = styled.button`
-    background-color: #4CAF50; /* Green */
-    border: none;
-    color: white;
-    padding: 15px 32px;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-    font-size: 16px;
-    margin: 4px 2px;
-    cursor: pointer;
+const Button = styled.button`
+  background-color: ${(props) => props.primary && "palevioletred"},
+    ${(props) => props.secondary && "green"};
+  border: none;
+  color: ${(props) => (props.primary && "white")}, ${(props) => (props.secondary && "white")};
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
 `;
+
+export default Button;
